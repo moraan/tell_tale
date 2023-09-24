44502d30c71c93258d00e5124d2bd2da2db03fdc
Herman <hcontr4@uic.edu>
2023-09-24 15:07:44 -0500

    Added route for diagram

---
+++ src/components/Diagram.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import diagramImage from '../assets/diagram.svg';
+
+function Diagram() {
+  return <img src={diagramImage} alt="Diagram" />;
+}
+
+export default Diagram;
---
+++ src/components/Landing.tsx
@@ -0,0 +1,16 @@
+// Landing.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Landing() {
+  return (
+    <div className="landing-container">
+      <h1>Welcome to the Landing Page</h1>
+      <Link to="/diagram">
+        <button>Go to Diagram</button>
+      </Link>
+    </div>
+  );
+}
+
+export default Landing;
