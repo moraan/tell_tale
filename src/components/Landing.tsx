021196274bf1be83f2cb1566898a342c2d8fb48a
Herman <hcontr4@uic.edu>
2023-10-29 16:36:50 -0500

    Working signup and login

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,13 +1,16 @@
-// Landing.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Landing() {
+  console.log();
   return (
     <div className="landing-container">
       <h1>Welcome to the Landing Page</h1>
-      <Link to="/diagram">
-        <button>Go to Diagram</button>
+      <Link to="/login">
+        <button>Login</button>
+      </Link>
+      <Link to="/signup">
+        <button>Signup</button>
       </Link>
     </div>
   );
---
+++ src/components/Signup.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import supabase from "../config/supabaseClient";
+
+function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+  };
+
+  return (
+    <div className="signup-container">
+      <h2>Signup</h2>
+      <form onSubmit={handleSignup}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
