b1534469df98d9db202fa097e1e7f7a654f5bce4
Alex Moran <alxmoraan@gmail.com>
2023-12-02 00:00:16 -0600

    add landing description

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -22,6 +22,9 @@ function Landing() {
   return (
     <div className="landing-container">
       <h1>Welcome to Tell Tale</h1>
+      <p className="intro-description">
+        Welcome to our immersive storytelling hub! Immerse yourself in the enchanting world of our carefully curated collection of three captivating children's stories. Sign in to explore these timeless tales, personalize your story experience, choose trigger words for sounds, and let our voice recognition feature track your spoken words. Discover storytelling in a whole new way!
+      </p>
       <Link to="/login">
         <button>Login</button>
       </Link>
